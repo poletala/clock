8f6d0ddaa8775b4b66a631be8295d598b2bf384d
Alena <92329824+poletala@users.noreply.github.com>
2024-03-22 12:54:29 +0500

    correct spaces

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
-
 const getCurrentTimeDate = () => {
     let currentTimeDate = new Date();
+
     let month = new Array();
     month[0] = "January";
     month[1] = "February";
@@ -15,18 +14,25 @@ const getCurrentTimeDate = () => {
     month[9] = "October";
     month[10] = "November";
     month[11] = "December";
+
 let hours   =  currentTimeDate.getHours();
 let minutes =  currentTimeDate.getMinutes();
 minutes = minutes < 10 ? '0'+minutes : minutes;
 let seconds = currentTimeDate.getSeconds();
 seconds = seconds < 10 ? '0'+seconds : seconds;
+
 let currentTime = `${hours}:${minutes}:${seconds}`;
+
 let currentDate  = currentTimeDate.getDate();
 let currentMonth = month[currentTimeDate.getMonth()];
 let CurrentYear = currentTimeDate.getFullYear();
+
 let fullDate = `${currentDate} ${currentMonth}, ${CurrentYear}`;
+
 document.querySelector("time").innerHTML = currentTime;
 document.querySelectorAll("time")[1].innerHTML = fullDate;
+
 setTimeout(getCurrentTimeDate, 500);
 }
+
 getCurrentTimeDate();
